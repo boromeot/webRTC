4ae0e86e8de4ac31a4fec6a6dfe8d75395df51d5
boromeot <boromeotyler@gmail.com>
2022-11-08 14:57:14 -0800

    got user webcam video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeNEnIkBp69KtRx3wE6pzA-PnYWjPPLxk",
+  authDomain: "webrtc-aa96f.firebaseapp.com",
+  projectId: "webrtc-aa96f",
+  storageBucket: "webrtc-aa96f.appspot.com",
+  messagingSenderId: "1080578656305",
+  appId: "1:1080578656305:web:85309e3a06217747076d10",
+  measurementId: "G-SKY7HDM5NJ"
+};
+
+const servers= {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+}
+let pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+const webCam = document.getElementById('webCam');
+const remoteCam = document.getElementById('remoteCam');
+
+const startWebCam = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true});
+  remoteStream = new MediaStream();
+
+  localStream.getTracks().forEach(track => {
+    pc.addTrack(track, localStream);
+  });
+
+  pc.ontrack = event => {
+    event.streams[0].getTracks().forEach(track => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webCam.srcObject = localStream;
+  remoteCam.srcObject = remoteStream;
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="webCam"></video>
+      <video id="remoteCam"></video>
+      <button onClick={startWebCam}>Start webcam</button>
     </div>
   );
 }
